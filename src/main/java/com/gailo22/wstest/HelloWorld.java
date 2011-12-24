8c6e8c727e34cab4e4c41cca62ab217167669ec3
gailo22 <gailo22@gmail.com>
2011-12-25 00:04:05 +0800

    added jetty server

---
+++ src/main/java/com/gailo22/wstest/HelloWorld.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gailo22.wstest;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+
+import java.io.IOException;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+
+public class HelloWorld extends AbstractHandler {
+
+    public void handle(String target,
+            Request baseRequest,
+            HttpServletRequest request,
+            HttpServletResponse response)
+            throws IOException, ServletException {
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        baseRequest.setHandled(true);
+        response.getWriter().println("<h1>Hello World</h1>");
+    }
+
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(9999);
+        server.setHandler(new HelloWorld());
+
+        server.start();
+        server.join();
+    }
+}
--- src/main/java/com/gailo22/wstest/JettyFixtureServer.java
+++ src/main/java/com/gailo22/wstest/JettyFixtureServer.java
@@ -4,24 +4,41 @@
  */
 package com.gailo22.wstest;
 
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.bio.SocketConnector;
+import org.eclipse.jetty.webapp.WebAppContext;
+
 /**
  *
  * @author User
  */
 public class JettyFixtureServer {
 
-    public static void main(String[] args) {
-        /* Server server = new Server(8080);
+    public static void main(String[] args) throws Exception {
+        Server jettyServer = null;
+        try {
+            jettyServer = new Server();
 
-        ContextHandler context = new ContextHandler();
-        context.setContextPath("/hello");
-        context.setResourceBase(".");
-        context.setClassLoader(Thread.currentThread().getContextClassLoader());
-        server.setHandler(context);
+            SocketConnector conn = new SocketConnector();
+            conn.setPort(9999);
+            jettyServer.setConnectors(new Connector[]{conn});
 
-        context.setHandler(new HelloHandler());
+            WebAppContext wah = new WebAppContext();
+            wah.setContextPath("/ws-test");
+            wah.setWar("src/main/webapp");
+            jettyServer.setHandler(wah);
 
-        server.start();
-        server.join(); */
+            jettyServer.start();
+        } catch (Exception e) {
+            System.out.println("Could not start the Jetty server: " + e);
+            if (jettyServer != null) {
+                try {
+                    jettyServer.stop();
+                } catch (Exception e1) {
+                    System.out.println("Unable to stop the jetty server: " + e1);
+                }
+            }
+        }
     }
 }
